615953574d9b9c387fa54dfdb594fdb0d2bf2913
Fedor B Gorsky <fedor@support-pc.org>
2020-01-16 15:09:08 +0300

    IDE stubs

---
+++ stubs/Mapper.php
@@ -0,0 +1,18 @@
+<?php
+namespace A2o;
+
+class Mapper
+{
+
+    /**
+     * @param array $source
+     * <p>
+     * Source array (name-value pairs)
+     * </p>
+     * @param mixed $receiver
+     * <p>
+     * Object that receive props from source array
+     * </p>
+     */
+    public static function map(array $source, $receiver) : void { }
+}
